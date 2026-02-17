af587fe63b37ce828330c9914809c8c891bb5225
Ali <aaliyev14291@ada.edu.az>
2026-02-17 14:06:36 +0400

    ManyToOne

---
+++ src/main/java/com/ltc/jpa/model/StudentEntity.java
@@ -0,0 +1,31 @@
+package com.ltc.jpa.model;
+
+import com.ltc.jpa.model.enumaration.CourseStatus;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table (name = "Students")
+public class StudentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)  // .ORDINAL returns 0, 1
+    private CourseStatus courseStatus;
+
+    @Column(length = 10, nullable = false, name = "project_name")
+    private String name;
+
+    @Column(length = 20, nullable = false)
+    private String surname;
+
+
+}
