e272e1f2c75b5116bf725acbffa3b52249b9ec5a
Ali <aaliyev14291@ada.edu.az>
2026-02-21 09:38:54 +0400

    Implementation

--- src/main/java/com/ltc/jpa/service/impl/CourseServiceImpl.java
+++ src/main/java/com/ltc/jpa/service/impl/CourseServiceImpl.java
@@ -15,3 +15,36 @@ import org.springframework.web.bind.annotation.PathVariable;
 //Create Student Entity, set RequestDTO, save db
 //Create Response Entity, set to saved db, return response
 
+@Service
+public class CourseServiceImpl implements CourseService {
+
+
+    private final CourseRepo courseRepo;
+
+    public CourseServiceImpl(CourseRepo courseRepo) {
+        this.courseRepo = courseRepo;
+    }
+
+    @Override
+    public CourseResponseDTO create(CourseRequestDTO courseRequestDTO) {
+
+        CourseEntity courseEntity = new CourseEntity();
+
+        courseEntity.setCourseName(courseRequestDTO.getCourseName());
+        courseEntity.setCourseCode(courseRequestDTO.getCourseCode());
+        courseEntity.setStudentName(courseRequestDTO.getStudentName());
+        courseEntity.setStudentSurname(courseRequestDTO.getStudentSurname());
+
+        CourseEntity courseDB = courseRepo.save(courseEntity);
+
+        CourseResponseDTO courseResponseDTO = new CourseResponseDTO();
+
+        courseResponseDTO.setId(courseDB.getId());
+        courseResponseDTO.setCourseName(courseDB.getCourseName());
+        courseResponseDTO.setCourseCode(courseDB.getCourseCode());
+        courseResponseDTO.setStudentName(courseDB.getStudentName());
+        courseResponseDTO.setStudentSurname(courseDB.getStudentSurname());
+
+        return courseResponseDTO;
+    }
+}
